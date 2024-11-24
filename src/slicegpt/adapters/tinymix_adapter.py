9f8f0557e78c2f10f721d194bd9186dcdb00fc8c
Aucki6144 <aucki6144@163.com>
2024-11-24 17:35:56 +0800

    Feature: support for tinymix

---
+++ src/slicegpt/adapters/tinymix_adapter.py
@@ -0,0 +1,267 @@
+# -*- coding:utf-8 -*-　
+# Last modify: Liu Wentao
+# Description: Adapter for TinyMix 1x8B
+# Note:
+
+from typing import Optional, Tuple, Sequence
+
+from torch import Tensor, FloatTensor
+from torch.nn import Linear, Module
+from transformers import MixtralForCausalLM, PretrainedConfig, MixtralConfig
+from transformers.models.mixtral.modeling_mixtral import MixtralDecoderLayer, MixtralRMSNorm
+import torch
+
+from slicegpt import LayerAdapter, ModelAdapter
+
+
+class CompressedMixtralDecoderLayer(MixtralDecoderLayer):
+
+    def forward(
+            self,
+            hidden_states: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            position_ids: Optional[torch.LongTensor] = None,
+            past_key_value: Optional[Tuple[torch.Tensor]] = None,
+            output_attentions: Optional[bool] = False,
+            output_router_logits: Optional[bool] = False,
+            use_cache: Optional[bool] = False,
+            cache_position: Optional[torch.LongTensor] = None,
+            **kwargs,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+
+        """
+        Args:
+            hidden_states (`torch.FloatTensor`): input to the layer of shape `(batch, seq_len, embed_dim)`
+            attention_mask (`torch.FloatTensor`, *optional*): attention mask of size
+                `(batch, sequence_length)` where padding elements are indicated by 0.
+            past_key_value (`Tuple(torch.FloatTensor)`, *optional*): cached past key and value projection states
+            output_attentions (`bool`, *optional*):
+                Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+                returned tensors for more detail.
+            output_router_logits (`bool`, *optional*):
+                Whether or not to return the logits of all the routers. They are useful for computing the router loss, and
+                should not be returned during inference.
+            use_cache (`bool`, *optional*):
+                If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding
+                (see `past_key_values`).
+            cache_position (`torch.LongTensor` of shape `(sequence_length)`, *optional*):
+                Indices depicting the position of the input sequence tokens in the sequence.
+            kwargs (`dict`, *optional*):
+                Arbitrary kwargs to be ignored, used for FSDP and other methods that injects code
+                into the model
+        """
+
+        residual = hidden_states
+
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Self Attention
+        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+            cache_position=cache_position,
+        )
+
+        if hasattr(self, "attn_shortcut_Q") and self.attn_shortcut_Q is not None:
+            rotated_residual = torch.matmul(residual, self.attn_shortcut_Q)
+            hidden_states = rotated_residual + hidden_states
+        else:
+            hidden_states = residual + hidden_states
+
+        # Fully Connected
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states, router_logits = self.block_sparse_moe(hidden_states)
+
+        if hasattr(self, "mlp_shortcut_Q") and self.mlp_shortcut_Q is not None:
+            rotated_residual = torch.matmul(residual, self.mlp_shortcut_Q)
+            hidden_states = rotated_residual + hidden_states
+        else:
+            hidden_states = residual + hidden_states
+
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (self_attn_weights,)
+
+        if use_cache:
+            outputs += (present_key_value,)
+
+        if output_router_logits:
+            outputs += (router_logits,)
+
+        return outputs
+
+
+class MixtralLayerAdapter(LayerAdapter):
+
+    def __init__(self, layer: MixtralDecoderLayer) -> None:
+        super().__init__()
+        self._layer: MixtralDecoderLayer = layer
+
+    @property
+    def layer(self) -> Module:
+        return self._layer
+
+    @property
+    def hidden_states_args_position(self) -> int:
+        return 0
+
+    @property
+    def hidden_states_output_position(self) -> int:
+        return 0
+
+    def get_first_layernorm(self) -> Module:
+        return self.layer.input_layernorm
+
+    def get_second_layernorm(self) -> Module:
+        return self.layer.post_attention_layernorm
+
+    def get_attention_inputs(self) -> Sequence[Linear]:
+        return [self.layer.self_attn.q_proj, self.layer.self_attn.k_proj, self.layer.self_attn.v_proj]
+
+    def get_attention_output(self) -> Linear:
+        return self.layer.self_attn.o_proj
+
+    def get_mlp_inputs(self) -> Sequence[Linear]:
+        return [self.layer.mlp.gate_proj, self.layer.mlp.up_proj]
+
+    def get_mlp_output(self) -> Linear:
+        return self.layer.mlp.down_proj
+
+
+class MixtralModelAdapter(ModelAdapter):
+
+    def __init__(self, model: MixtralForCausalLM) -> None:
+        super().__init__()
+        self._model: MixtralForCausalLM = model
+
+    @property
+    def model(self) -> Module:
+        return self._model
+
+    @property
+    def config(self) -> PretrainedConfig:
+        return self._model.config
+
+    @property
+    def config_type(self) -> type:
+        return MixtralConfig
+
+    @property
+    def parallel_blocks(self) -> bool:
+        return False
+
+    @property
+    def seqlen(self) -> int:
+        return self.config.max_position_embeddings
+
+    @property
+    def hidden_size(self) -> int:
+        return self.config.hidden_size
+
+    @property
+    def should_bake_mean_into_linear(self) -> bool:
+        return False
+
+    @property
+    def original_layer_type(self) -> type:
+        return MixtralDecoderLayer
+
+    @property
+    def original_layer_norm_type(self) -> type:
+        return MixtralRMSNorm
+
+    @property
+    def layer_adapter_type(self) -> type:
+        return MixtralLayerAdapter
+
+    @property
+    def compressed_layer_type(self) -> type:
+        return CompressedMixtralDecoderLayer
+
+    @property
+    def use_cache(self) -> bool:
+        return self.config.use_cache
+
+    @use_cache.setter
+    def use_cache(self, value: bool) -> None:
+        self.config.use_cache = value
+
+    def compute_output_logits(self, input_ids: Tensor) -> FloatTensor:
+        return self.model(input_ids=input_ids).logits
+
+    def convert_layer_to_compressed(self, layer: Module, layer_idx: int | None) -> Module:
+        compressed_layer = CompressedMixtralDecoderLayer(self._model.config, layer_idx).to(
+            self._model.config.torch_dtype)
+        compressed_layer.load_state_dict(layer.state_dict(), strict=True)
+        return compressed_layer
+
+    def get_layers(self) -> Sequence[LayerAdapter]:
+        return [MixtralLayerAdapter(layer) for layer in self.model.model.layers]
+
+    def get_raw_layer_at(self, index: int) -> Module:
+        return self.model.model.layers[index]
+
+    def set_raw_layer_at(self, index: int, new_layer: Module) -> None:
+        self.model.model.layers[index] = new_layer
+
+    def get_embeddings(self) -> list[Module]:
+        return [self.model.model.embed_tokens]
+
+    def get_pre_head_layernorm(self) -> Module:
+        pre_head_layernorm = self.model.model.norm
+        assert isinstance(pre_head_layernorm, self.original_layer_norm_type)
+        return pre_head_layernorm
+
+    def get_lm_head(self) -> Linear:
+        return self.model.lm_head
+
+    @classmethod
+    def _from_pretrained(
+            cls,
+            model_name: str,
+            model_path: str,
+            *,
+            dtype: torch.dtype = torch.float16,
+            local_files_only: bool = False,
+            token: str | bool | None = None
+    ) -> ModelAdapter | None:
+        if not model_name.startswith("eastwind/tinymix-8x1b-chat"):
+            return None
+
+        model = MixtralForCausalLM.from_pretrained(
+            model_path, torch_dtype=dtype, token=token, local_files_only=local_files_only
+        )
+        model.config.torch_dtype = dtype
+        return cls(model)
+
+    @classmethod
+    def _from_uninitialized(
+            cls,
+            model_name: str,
+            model_path: str,
+            *,
+            dtype: torch.dtype = torch.float16,
+            local_files_only: bool = False,
+            token: str | bool | None = None
+    ) -> ModelAdapter | None:
+        if not model_name.startswith("eastwind/tinymix-8x1b-chat"):
+            return None
+
+        class UninitializedMixtralForCausalLM(MixtralForCausalLM):
+            def _init_weights(self, _) -> None:
+                # Prevent weight initialization
+                pass
+
+        config = MixtralConfig.from_pretrained(
+            model_path, torch_dtype=dtype, token=token, local_files_only=local_files_only
+        )
+        model = UninitializedMixtralForCausalLM(config)
+        model = model.to(dtype=dtype)
+
+        return MixtralModelAdapter(model)
